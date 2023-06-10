a7eab9b42fcde8cc933511eb9a5a09060c383d85
jeunh <gkwldms1218@gmail.com>
2023-06-10 18:21:45 +0900

    트랜지션 구현하기

--- src/components/Dialog.js
+++ src/components/Dialog.js
@@ -1,6 +1,42 @@
-import React from "react";
-import styled from 'styled-components';
+import React, { useEffect, useState } from "react";
+import styled, { css, keyframes } from 'styled-components';
 import Button from "./Button";
+
+const fadeIn = keyframes `
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`;
+
+const fadeOut = keyframes `
+  from {
+    opacity: 1;
+  }
+  to {
+    opacity: 0;
+  }
+`;
+
+const slideUp = keyframes `
+  from {
+    transform: translateY(200px);
+  }
+  to {
+    transform: translateY(0px);
+  }
+`;
+
+const slideDown = keyframes `
+  from {
+    transform: translateY(0px);
+  }
+  to {
+    transform: translateY(200px);
+  }
+`;
 
 const DarkBackground = styled.div `
   position: fixed;
@@ -12,6 +48,15 @@ const DarkBackground = styled.div `
   align-items: center;
   justify-content: center;
   background: rgba(0,0,0,0.8);
+
+  animation-duration: 0.25s;
+  animation-timing-function: ease-out;
+  animation-name: ${fadeIn};
+  animation-fill-mode: forwards;
+
+  ${props => props.disappear && css `
+    animation-name: ${fadeOut};
+  `}
 `;
 
 const DialogBlock = styled.div `
@@ -26,6 +71,15 @@ const DialogBlock = styled.div `
   p {
     font-size: 1.125rem;
   }
+
+  animation-duration: 0.25s;
+  animation-timing-function: ease-out;
+  animation-name: ${slideUp};
+  animation-fill-mode: forwards;
+
+  ${props => props.disappear && css `
+    animation-name: ${slideDown};
+  `}
 `;
 
 const ButtonGroup = styled.div`
@@ -41,10 +95,22 @@ const ShortMarginButton = styled(Button) `
 `;
 
 function Dialog({ title, children, confirmText, cancelText, onConfirm, onCancel, visible }) {
-  if(!visible) return null;
+  const [animate, setAnimate] = useState(false);
+  const [localVisible, setLocalVisible] = useState(animate);
+
+  useEffect(() => {
+    // visible 값이 true => false 가 되는 것을 감지
+    if(localVisible && !visible) {
+      setAnimate(true);
+      setTimeout(() => setAnimate(false), 250);
+    }
+    setLocalVisible(visible);
+  }, [localVisible, visible]);
+
+  if (!animate && !localVisible) return null;
   return (
-    <DarkBackground>
-      <DialogBlock>
+    <DarkBackground disappear={!visible}>
+      <DialogBlock disappear={!visible}>
         <h3>{title}</h3>
         <p>{children}</p>
         <ButtonGroup>
